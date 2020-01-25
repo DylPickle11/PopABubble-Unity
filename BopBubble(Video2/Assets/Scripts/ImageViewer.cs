5cccfd4584270499f292b65840ba25b2bbfaa019
Dylan <Dylan.Griff11@gmail.com>
2020-01-25 11:50:07 -0600

    First commit first two videos

---
+++ BopBubble(Video2/Assets/Scripts/ImageViewer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //Access to rawimagetype
+
+public class ImageViewer : MonoBehaviour
+{
+    public MeasureDepth mMeasureDepth;
+
+    //Source of Data Gives us color and depth data
+    public MultiSourceManager mMultiSource;
+
+    // Output the color we are getting from the kinect
+    public RawImage mRawImage;
+
+    public RawImage mRawDepth;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        mRawImage.texture = mMultiSource.GetColorTexture();
+
+        mRawDepth.texture = mMeasureDepth.mDepthTexture;
+    }
+}
---
+++ BopBubble(Video2/Assets/Scripts/MeasureDepth.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Windows.Kinect;
+
+public class MeasureDepth : MonoBehaviour
+{
+
+    public MultiSourceManager mMultiSource;
+    public Texture2D mDepthTexture;
+
+    private ushort[] mDepthData = null;
+
+    private CameraSpacePoint[] mCameraSpacePoints = null;
+    private ColorSpacePoint[] mColorSpacePoints = null;
+
+    private readonly Vector2Int mDepthResolution = new Vector2Int(512, 424);
+
+
+    private KinectSensor mSensor = null;
+    private CoordinateMapper mMapper = null;
+
+
+    private void Awake()
+    {
+        mSensor = KinectSensor.GetDefault();
+        mMapper = mSensor.CoordinateMapper;
+
+        int arraySize = mDepthResolution.x * mDepthResolution.y;
+
+        mCameraSpacePoints = new CameraSpacePoint[arraySize];
+        mColorSpacePoints = new ColorSpacePoint[arraySize];
+
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DepthToColor();
+            mDepthTexture = CreateTexture();
+        }
+
+    }
+
+
+
+    //taking the camera space points or the color space points and setting and lining them up
+    private void DepthToColor()
+    {
+        //Get depth
+        mDepthData = mMultiSource.GetDepthData();
+
+        //Map
+
+        mMapper.MapDepthFrameToCameraSpace(mDepthData, mCameraSpacePoints);
+        mMapper.MapDepthFrameToColorSpace(mDepthData, mColorSpacePoints);
+    }
+    //Filter
+    private Texture2D CreateTexture()
+    {
+
+        Texture2D newTexture = new Texture2D(1920, 1080, TextureFormat.Alpha8, false);
+        for (int x = 0; x < 1920; x++)
+        {
+            for (int y = 0; y < 1080; y++)
+            {
+                // Set pixels to Clear
+                newTexture.SetPixel(x, y, Color.clear);
+            }
+        }
+
+        foreach (ColorSpacePoint point in mColorSpacePoints)
+        {
+            newTexture.SetPixel((int)point.X, (int)point.Y, Color.black);
+        }
+        newTexture.Apply();
+        return newTexture;
+
+    }
+}
+
+
